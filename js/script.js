7b68b8b307cbcb23a2c726635a019ae9ea879a71
Alessandro-Rega <regaale47@gmail.com>
2021-11-10 15:30:03 +0100

    insert gioco dei dadi

--- js/script.js
+++ js/script.js
@@ -12,11 +12,20 @@ let emailValide = [
     "arlecchino@gmail.com",
     "matteo_bernardeschi@gmail.com",
 ];
-document.getElementById("invio").addEventListener("click", function(){
+
+// funzione controllo click
+document.getElementById("invioMail").addEventListener("click", function(){
+
+    // dichiaro le costanti
     const mail = document.getElementById("email").value;
     const esito_mail = document.getElementById("esito-mail");
+
+    // resetto l'esito
     esito_mail.innerHTML = "Esito: ";
+
+    // controllo se l'email è valida
     if(controlloMail.test(mail)==true){
+        // controllo se l'email inserita è nel database
         for(let i = 0; i < emailValide.length; i++){
             if(mail === emailValide[i]){
                 esito_mail.innerHTML += " Accesso Autorizzato"
@@ -32,7 +41,8 @@ document.getElementById("invio").addEventListener("click", function(){
     }
 });
 
-document.getElementById("reset").addEventListener("click", function(){
+// funzione di reset totale
+document.getElementById("resetMail").addEventListener("click", function(){
     document.getElementById("email").value = '';
     document.getElementById("esito-mail").innerHTML = "Esito: ";
 });
@@ -41,3 +51,41 @@ document.getElementById("reset").addEventListener("click", function(){
 // -----------------------------------------------------------------------------------------
 
 // GIOCO DEI DADI
+document.getElementById("tiraDadi").addEventListener("click", function(){
+    
+    // dichiaro le costanti
+    const player = document.getElementById("num-p");
+    const computer = document.getElementById("num-c");
+    const esitoDadi = document.getElementById("esito-dadi");
+
+    // resetto l'html
+    player.innerHTML = "Numero Giocatore:";
+    computer.innerHTML = "Numero Computer:";
+    esitoDadi.innerHTML = "Esito: ";
+
+    // genero i numeri random
+    let numeroGiocatore = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    let numeroComputer = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+
+    // stampo i numeri
+    player.innerHTML += (' ' + numeroGiocatore);
+    computer.innerHTML += (' ' + numeroComputer);
+
+    // controllo chi ha vinto
+    if(numeroGiocatore > numeroComputer){
+        esitoDadi.innerHTML += " Hai vinto";
+    }
+    else if (numeroGiocatore < numeroComputer){
+        esitoDadi.innerHTML += " Hai perso";
+    }
+    else {
+        esitoDadi.innerHTML += " Pareggio";
+    }
+});
+
+// funzione di reset totale
+document.getElementById("resetDadi").addEventListener("click", function(){
+    document.getElementById("num-p").innerHTML = "Numero Giocatore:";
+    document.getElementById("num-c").innerHTML = "Numero Computer:";
+    document.getElementById("esito-dadi").innerHTML = "Esito: ";
+});
